4aa9389389ec28215bf4cc4219403cbef6e87329
Kole Krstev <kole.krstev@gmail.com>
2020-05-13 10:41:32 -0700

    created new python script that prints 2 matrices

---
+++ samplescript.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+my_matrix = np.eye(4)
+my_second_matrix = np.array([1,2,3,4],
+                            [2,3,4,5],
+                            [4,3,6,8],
+                            [1,4,5,6]]
+
+print(my_matrix)
+print(my_second_matrix)
